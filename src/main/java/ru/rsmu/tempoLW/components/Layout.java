719d1360fb76354a53ba4501f8063c7011f44745
pavelpossiblep <46157984+pavelpossiblep@users.noreply.github.com>
2019-04-25 17:22:12 +0300

    1. Added locale toggle button (displayed differently: for unlogged user - on navbar next to "Log in", for logged user - in user menu) 2. Added title on hover on RNRMU logo (to help user understand its purpose)

--- src/main/java/ru/rsmu/tempoLW/components/Layout.java
+++ src/main/java/ru/rsmu/tempoLW/components/Layout.java
@@ -6,11 +6,15 @@ import org.apache.tapestry5.annotations.Log;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.LocalizationSetter;
 import org.tynamo.security.services.SecurityService;
 import ru.rsmu.tempoLW.entities.auth.User;
 import ru.rsmu.tempoLW.pages.Index;
 import ru.rsmu.tempoLW.pages.Login;
 import ru.rsmu.tempoLW.services.SecurityUserHelper;
+
+import java.util.List;
+import java.util.Locale;
 
 /**
  * @author leonid.
@@ -30,9 +34,26 @@ public class Layout {
     @Inject
     private SecurityUserHelper securityUserHelper;
 
+    @Inject
+    @Property
+    private Locale currentLocale;
+
+    @Inject
+    private LocalizationSetter localizationSetter;
+
     public String getUsername() {
         User user = securityUserHelper.getCurrentUser();
         return user != null ? user.getFirstName() + " " + (user.getMiddleName() != null ? user.getMiddleName() : "") : "";
+    }
+
+
+    /** switch app localization */
+    public void onTogglelocale() {
+        if(currentLocale.toString().equals("en")) {
+            localizationSetter.setLocaleFromLocaleName("ru");
+        } else {
+            localizationSetter.setLocaleFromLocaleName("en");
+        }
     }
 
     /**
